a27026fdf7f738c83ae4f621c271fc02ef2e9746
dmittrey <dmitryzubakhin@mail.ru>
2023-05-04 00:37:41 +0300

    Complete findKthStats and insert ops

--- cartesian_tree.cc
+++ cartesian_tree.cc
@@ -2,53 +2,110 @@
 
 namespace Trees
 {
-    CartesianNode *CartesianTree::merge(CartesianNode *lTree, CartesianNode *rTree)
+    CartesianTree::CartesianNode *CartesianTree::merge(CartesianNode *lTree, CartesianNode *rTree)
     {
+        if (lTree == nullptr && rTree == nullptr)
+            return nullptr;
         if (lTree == nullptr)
             return rTree;
         if (rTree == nullptr)
             return lTree;
 
-        if (lTree->prior_ > rTree->prior_)
+        if (lTree->prior_ >= rTree->prior_)
         {
-            lTree->rNode = merge(lTree->rNode, rTree);
+            lTree->r = merge(lTree->r, rTree);
+            lTree->recalc();
             return lTree;
         }
         else
         {
-            rTree->lNode = merge(lTree, rTree->lNode);
+            rTree->l = merge(lTree, rTree->l);
+            rTree->recalc();
             return rTree;
         }
     }
 
-    CartesianTree::split_pair CartesianTree::split(CartesianNode *tree, int index)
+    std::pair<CartesianTree::CartesianNode *, CartesianTree::CartesianNode *> CartesianTree::split(CartesianNode *tree, int index)
     {
         if (tree == nullptr)
-            return {0, 0};
+            return {nullptr, nullptr};
 
         if (index >= tree->key_) //  Ключ в правом поддереве
         {
-            CartesianTree::split_pair q = split(tree->rNode, index);
-            tree->rNode = q.first;
-            return {tree, q.second};
+            auto [l, r] = split(tree, index);
+            tree->r = l;
+            tree->recalc();
+            return {tree, r};
         }
         else
         {
-            CartesianTree::split_pair q = split(tree->lNode, index);
-            tree->lNode = q.second;
-            return {q.first, tree};
+            auto [l, r] = split(tree, index);
+            tree->l = r;
+            tree->recalc();
+            return {l, tree};
         }
     }
 
-    void CartesianTree::insert(int key)
+    CartesianTree::CartesianNode *CartesianTree::insert(CartesianNode *tree, CartesianNode *node) noexcept
     {
+        if (tree == nullptr)
+        {
+            return node;
+        }
+
+        if (node->prior_ > tree->prior_)
+        {
+            auto [r, l] = split(tree, node->key_);
+            node->l = l;
+            node->r = r;
+            node->recalc();
+            return node;
+        }
+        else
+        {
+            if (node->key_ >= tree->key_)
+            {
+                tree->r = insert(tree->r, node);
+            }
+            else
+            {
+                tree->l = insert(tree->l, node);
+            }
+            tree->recalc();
+            return tree;
+        }
     }
 
-    CartesianTree::iterator CartesianTree::findKthStats() const
+    int CartesianTree::size(CartesianNode *node) noexcept
     {
+        return (node == nullptr) ? 0 : node->size_;
     }
 
-    int CartesianTree::countLess(int upperBound) const
+    int CartesianTree::findKthStats(int k) const
     {
+        CartesianNode *cur = top;
+        while (cur != nullptr)
+        {
+            int sizeLeft = size(cur->l);
+
+            if (sizeLeft == k)
+                return cur->key_;
+
+            cur = (sizeLeft > k) ? cur->l : cur->r;
+            if (sizeLeft < k)
+                k -= sizeLeft + 1;
+        }
+
+        throw std::invalid_argument("Element not found");
+    }
+
+    int CartesianTree::countLess(int upperBound) const noexcept
+    {
+        
+    }
+
+    void CartesianTree::insert(int key) noexcept
+    {
+        top = insert(top, new CartesianNode(key));
     }
 }
