5b36847160bba57f18c348617e221545426a1bc8
dmittrey <dmitryzubakhin@mail.ru>
2023-05-04 00:36:55 +0300

    Migrate CartesianNode decl into Tree class

--- cartesian_tree.hpp
+++ cartesian_tree.hpp
@@ -1,31 +1,55 @@
 #pragma once
 
-#include "cartesian_node.hpp"
+#include <random>
 
 namespace Trees
 {
     class CartesianTree
     {
-    public:
-        using iterator = CartesianNode *;
+        struct CartesianNode
+        {
+            int key_;
+            int prior_ = std::rand();
+            int size_ = 1; /* Размер поддерева */
+
+            CartesianNode *l = nullptr;
+            CartesianNode *r = nullptr;
+
+            CartesianNode(int key) : key_(key) {}
+
+            /* Метод для пересчёта полей вершины */
+            void recalc() noexcept
+            {
+                size_ = 1;
+
+                if (l != nullptr)
+                    size_ += l->size_;
+                if (r != nullptr)
+                    size_ += r->size_;
+            }
+        };
 
     private:
-        using split_pair = std::pair<CartesianNode *, CartesianNode *>;
+        CartesianNode *top = nullptr;
 
-    private:
-        CartesianNode *top;
+    public: // Конструкторы
+        CartesianTree(int key) : top(new CartesianNode(key)) {}
 
     private: // Обслуживающие
         /* Метод для разделения дерева по ключу(ключ в правом поддереве) */
         static CartesianNode *merge(CartesianNode *lTree, CartesianNode *rTree);
         /* Метод для слияния двух деревьев */
-        static split_pair split(CartesianNode *tree, int index);
+        static std::pair<CartesianNode *, CartesianNode *> split(CartesianNode *tree, int index);
+        /* Метод для вставки */
+        static CartesianNode *insert(CartesianNode *tree, CartesianNode *node) noexcept;
+        /* Метод для получения размера поддерева */
+        static int size(CartesianNode *node) noexcept;
 
     public: // Селекторы
-        iterator findKthStats() const;
-        int countLess(int upperBound) const;
+        int findKthStats(int k) const noexcept;
+        int countLess(int upperBound) const noexcept;
 
     public: // Модификаторы
-        void insert(int key);
+        void insert(int key) noexcept;
     };
 }
