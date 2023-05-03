932eceb83e19eb4d568032140465c06c5d92a6e9
dmittrey <dmitryzubakhin@mail.ru>
2023-05-03 04:22:34 +0300

    Update cartesian tree internals
    Update methods and refactor aliases

--- cartesian_tree.hpp
+++ cartesian_tree.hpp
@@ -6,12 +6,24 @@ namespace Trees
 {
     class CartesianTree
     {
+    public: 
+        using iterator = CartesianNode *;
+
+    private:
+        using split_pair = std::pair<CartesianTree &, CartesianTree &>;
+
     private:
         CartesianNode *top;
 
-    public:
+    private: // Обслуживающие
+        static CartesianTree &merge(CartesianTree &lTree, CartesianTree &rTree);
+        static split_pair split(CartesianTree& tree, int index);
+
+    public: // Селекторы
+        iterator findKthStats() const;
+        int countLess(int upperBound) const;
+
+    public: // Модификаторы
         void insert(int key);
-        int findKthStats();
-        int countLess(int upperBound);
-    }
+    };
 }
