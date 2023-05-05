7670a9c98d7e90f161898c18e18aa30c1402023e
dmittrey <dmitryzubakhin@mail.ru>
2023-05-05 07:30:11 +0300

    Add dtor's implementation

--- cartesian-tree-container/include/cartesian_tree.hpp
+++ cartesian-tree-container/include/cartesian_tree.hpp
@@ -24,17 +24,44 @@ namespace Trees
 
             CartesianNode(int key) : key_(key) {}
 
+            /* Метод для получения размера поддерева */
+            static int size(CartesianNode *node) noexcept;
+
             /* Метод для пересчёта полей вершины */
             void recalc() noexcept;
 
+            /* Методы подвешивающие вершины */
             void hangL(CartesianNode *node) noexcept;
             void hangR(CartesianNode *node) noexcept;
+
+            /**/
+            ~CartesianNode()
+            {
+                if (l != nullptr && r != nullptr)
+                {
+                    /* Правая вершина заменит нас */
+                    if (r->prior_ >= l->prior_)
+                    {
+                        /* Правое поддерево без изменений, а левое поддерево будет результатом слияния
+                            текущего левого поддерева и левого поддерева r*/
+                        r->hangL(merge(l, r->l));
+                        /* Поменяли родителя правого ребёнка на нашего родителя */
+                        r->root = root;
+                    }
+                    /* Левая вершина заменит нас (аналогичный алгоритм) */
+                    else
+                    {
+                        l->hangR(merge(l->r, r));
+                        l->root = root;
+                    }
+                }
+            }
         };
 
     private:
         CartesianNode *top = nullptr;
 
-    private: // Обслуживающие
+    private: // Операции с деревом
         /* Метод для разделения дерева по ключу(ключ в правом поддереве) */
         static CartesianNode *merge(CartesianNode *lTree, CartesianNode *rTree);
         /* Метод для слияния двух деревьев */
@@ -75,5 +102,20 @@ namespace Trees
 
     public: // Модификаторы
         void insert(int key) noexcept;
+
+        ~CartesianTree()
+        {
+            while (top->l != nullptr)
+            {
+                delete top->l;
+            }
+
+            while (top->r != nullptr)
+            {
+                delete top->r;
+            }
+
+            delete top;
+        }
     };
 }
