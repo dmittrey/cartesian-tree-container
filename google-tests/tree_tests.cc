6632a14c823a704632bc4aca024db27521f2eab2
dmittrey <dmitryzubakhin@mail.ru>
2023-05-13 06:35:05 +0300

    Fix test cases
    findKthStats is starting from 0

--- google-tests/tree_tests.cc
+++ google-tests/tree_tests.cc
@@ -6,19 +6,19 @@ using Trees::CartesianTree;
 
 TEST(CartesianTreeTests, Test1)
 {
-    Trees::CartesianTree<int> tree;
+    Trees::CartesianTree<int> *tree = new Trees::CartesianTree<int>();
 
-    tree.insert(12);
+    tree->insert(12);
 
-    EXPECT_EQ(tree.findKthStats(1), 12);
-    EXPECT_EQ(tree.countLess(13), 1);
+    EXPECT_EQ(tree->findKthStats(0), 12);
+    EXPECT_EQ(tree->countLess(13), 1);
 }
 
 TEST(CartesianTreeTests, Test2)
 {
     Trees::CartesianTree<int> tree;
 
-    EXPECT_EQ(tree.findKthStats(2), 0); // Exception
+    EXPECT_THROW(tree.findKthStats(1), std::invalid_argument);
     EXPECT_EQ(tree.countLess(10), 0);
 }
 
@@ -30,7 +30,7 @@ TEST(CartesianTreeTests, Test3)
     tree.insert(5);
     tree.insert(15);
 
-    EXPECT_EQ(tree.findKthStats(2), 10);
+    EXPECT_EQ(tree.findKthStats(1), 10);
     EXPECT_EQ(tree.countLess(5), 0);
     EXPECT_EQ(tree.countLess(10), 1);
 }
@@ -43,7 +43,7 @@ TEST(CartesianTreeTests, Test4)
     tree.insert(-8);
     tree.insert(-6);
 
-    EXPECT_EQ(tree.findKthStats(2), -6);
+    EXPECT_EQ(tree.findKthStats(1), -6);
     EXPECT_EQ(tree.countLess(-6), 1);
     EXPECT_EQ(tree.countLess(-4), 2);
 }
